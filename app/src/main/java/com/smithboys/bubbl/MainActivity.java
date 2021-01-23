a367e4c54fd20dc2ad5cb03455ad27fa15bb3d94
Gary Peng <gary-peng@users.noreply.github.com>
2021-01-23 01:51:10 -0500

    QR verification

--- app/src/main/java/com/smithboys/bubbl/MainActivity.java
+++ app/src/main/java/com/smithboys/bubbl/MainActivity.java
@@ -1,14 +1,31 @@
 package com.smithboys.bubbl;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.smithboys.bubbl.activities.QRActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configBtn();
+    }
+
+    private void configBtn() {
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, QRActivity.class));
+            }
+        });
     }
 }
+
